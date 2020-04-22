043dce2e6b056e2614570ec0b626c279774bc4b4
Bruno <siffli13@gmail.com>
2020-04-22 17:11:49 +0200

    ajout cache twig extension

---
+++ src/Twig/CacheExtension/CacheableInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Twig\CacheExtension;
+
+interface CacheableInterface
+{
+
+    public function getUpdatedAt(): \DateTimeInterface;
+
+    public function getId(): int;
+
+}
